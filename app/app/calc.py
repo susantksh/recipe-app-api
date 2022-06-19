808da6ee406dda24a5dc18584b9dffb8a414cb9a
susantksh <susantbm683@gmail.com>
2022-06-18 21:32:17 -0400

    test success

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,8 @@
 """
 adding two number
 """
+
+
 def add(x_num, y_num):
     """adding two numbers."""
     return x_num+y_num
@@ -8,6 +10,7 @@ def add(x_num, y_num):
 # """
 # subtracting two number
 # """
+
 def subtract(x_num, y_num):
     """
     subtracting
